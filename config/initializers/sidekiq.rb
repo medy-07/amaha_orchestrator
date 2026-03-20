ad9a6c1ae3f9a2f2acea11867b6421a8889a2927
Sunil Madde <maddesunil07@gmail.com>
2026-03-20 22:37:00 +0530

    Initial Rails setup with Sidekiq, Redis, MYSQL DB

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+redis_url = ENV.fetch("redis_url")
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
+end
