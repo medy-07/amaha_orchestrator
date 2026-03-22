b13609b477c4f6ea953708c22483017a71ee3ce5
Sunil Madde <maddesunil07@gmail.com>
2026-03-22 16:03:55 +0530

    Implemented SchedulerWorker to process queued jobs with priority ordering and Clientwise Fairness

---
+++ app/jobs/scheduler_worker.rb
@@ -0,0 +1,46 @@
+class SchedulerWorker
+  include Sidekiq::Worker
+
+  def perform
+    clients_jobs = fetch_jobs_grouped_by_client
+
+    clients_jobs.each do |client_id, jobs|
+      process_client_jobs(client_id, jobs)
+    end
+  end
+
+  private
+
+    def fetch_jobs_grouped_by_client
+      Job.queued
+        .order(priority: :desc, created_at: :asc)
+        .group_by(&:client_id)
+    end
+
+    def process_client_jobs(client_id, jobs)
+      client = Client.find(client_id)
+
+      available_slots = get_available_slots(client)
+
+      return if available_slots <= 0
+
+      jobs.first(available_slots).each do |job|
+        start_job(job)
+      end
+    end
+
+    def get_available_slots(client)
+      running_count = Job.where(client_id: client.id, status: :running).count
+      client.concurrency_limit - running_count
+    end
+
+    def start_job(job)
+      job.with_lock do
+        return unless job.queued?
+
+        job.start!
+
+        JobExecutorWorker.perform_async(job.id)
+      end
+    end
+end
